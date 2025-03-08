3e586c8b05700e00aae22e34eb8b9721bd9fb3aa
Miguel Guirao <mguirao@gmail.com>
2025-03-08 01:25:07 -0700

    initial commit

--- app/src/main/java/org/guirao/scaffold/MainActivity.kt
+++ app/src/main/java/org/guirao/scaffold/MainActivity.kt
@@ -1,17 +1,42 @@
 package org.guirao.scaffold
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.ShoppingCart
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import org.guirao.scaffold.ui.theme.ScaffoldTheme
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import kotlin.Int
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,11 +44,13 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ScaffoldTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                    topBar = { TopBar() },
+                    bottomBar = { BottomBar() },
+                    floatingActionButton = { Fab() }
+                ) { innerPadding ->
+                    Greeting(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
@@ -31,9 +58,54 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+private fun MainActivity.Fab() {
+    var presses: Int by remember { mutableIntStateOf(0) }
+    FloatingActionButton(onClick = {Log.d("FAB", presses++.toString())}) {
+        Icon(Icons.Default.Add, contentDescription = "Add")
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun MainActivity.BottomBar() {
+    BottomAppBar(
+        containerColor = MaterialTheme.colorScheme.primaryContainer,
+        contentColor = MaterialTheme.colorScheme.primary,
+        tonalElevation = 4.dp,
+    ) {
+        Text(
+            modifier = Modifier.fillMaxWidth(),
+            textAlign = TextAlign.Center,
+            text = "Mayan Doctor",
+            style = MaterialTheme.typography.titleMedium
+        )
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun MainActivity.TopBar() {
+    TopAppBar(
+        colors = topAppBarColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer,
+            titleContentColor = MaterialTheme.colorScheme.primary,
+        ),
+        title = { Text("Mayan Doctor") },
+        actions = {
+            IconButton(onClick = { /* doSomething() */ }) {
+                Icon(Icons.Filled.Delete, contentDescription = "Localized description")
+            };
+            IconButton(onClick = { /* doSomething() */ }) {
+                Icon(Icons.Filled.Email, contentDescription = "Localized description")
+            };
+            IconButton(onClick = { /* doSomething() */ }) {
+                Icon(Icons.Filled.ShoppingCart, contentDescription = "Localized description")
+            };
+        }
     )
+}
+
+@Composable
+fun Greeting(modifier: Modifier = Modifier) {
+    Log.d("Greeting", "Greeting called")
 }

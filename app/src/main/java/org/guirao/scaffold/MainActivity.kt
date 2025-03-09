c3fb8a0a44d89daf4b3a20ec119616bcbea04ed3
Miguel Guirao <mguirao@gmail.com>
2025-03-08 19:11:17 -0700

    initial commit

--- app/src/main/java/org/guirao/scaffold/MainActivity.kt
+++ app/src/main/java/org/guirao/scaffold/MainActivity.kt
@@ -5,14 +5,16 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.Check
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material.icons.filled.ShoppingCart
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -20,8 +22,10 @@ import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
 import androidx.compose.runtime.Composable
@@ -32,8 +36,7 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import org.guirao.scaffold.ui.theme.ScaffoldTheme
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import kotlin.Int
 
@@ -44,6 +47,9 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ScaffoldTheme {
+                // Our code already has a Scaffold component that we can use to wrap our content.
+                // First, we define which components we want to use in the Scaffold.
+                // In this case, we want to use the topBar, bottomBar, and floatingActionButton
                 Scaffold(
                     modifier = Modifier.fillMaxSize(),
                     topBar = { TopBar() },
@@ -57,8 +63,10 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+// For each component of the Scaffold, we define a separate composable function.
 @Composable
 private fun MainActivity.Fab() {
+    // presses is the state of the button.
     var presses: Int by remember { mutableIntStateOf(0) }
     FloatingActionButton(onClick = {Log.d("FAB", presses++.toString())}) {
         Icon(Icons.Default.Add, contentDescription = "Add")
@@ -107,5 +115,29 @@ private fun MainActivity.TopBar() {
 
 @Composable
 fun Greeting(modifier: Modifier = Modifier) {
-    Log.d("Greeting", "Greeting called")
+    var email: String by remember { mutableStateOf("") }
+    var password: String by remember { mutableStateOf("") }
+    Log.d("Mayan Doctor", "email: $email")
+    Column(
+        modifier = Modifier.padding(top = 90.dp)
+    ) {
+        Text(
+            text = "We can reach you at: $email",
+        );
+        OutlinedTextField(
+            value = email,
+            onValueChange = {email = it},
+            label = { Text("Email") },
+            leadingIcon = { Icon(Icons.Default.Email, contentDescription = "Email") }
+        );
+
+        Spacer(modifier = Modifier.padding(10.dp))
+
+        TextField(
+            value = password,
+            onValueChange = {password = it},
+            label = { Text("Password") },
+            leadingIcon = { Icon(Icons.Default.Search, contentDescription = "Password") }
+        )
+    }
 }
